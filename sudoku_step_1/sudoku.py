7ab970786e8c4d452f1d697513cf17285a1f7a23
mApS-bit <psmarco832@gmail.com>
2025-08-20 21:04:51 -0600

    Fix: Correct Board Geometry

--- sudoku_step_1/sudoku.py
+++ sudoku_step_1/sudoku.py
@@ -17,37 +17,38 @@ class SudokuBoard(tk.Tk):
         self.display = tk.Label(
             master=display_frame,
             text='Sudoku',
-            font=font.Font(size=28, weight='bold'),
+            font=font.Font(size=18, weight='bold'),
         )
         self.display.pack()
 
     def _create_board_grid(self):
         grid_frame =  tk.Frame(master=self)
-        grid_frame.pack()
+        grid_frame.pack(expand=True, fill=tk.BOTH)
         for row in range(9):
-            self.rowconfigure(row, weight=1, minsize=50)
-            self.columnconfigure(row, weight=1, minsize=75)
+            grid_frame.rowconfigure(row, weight=1, minsize=50)
+            grid_frame.columnconfigure(row, weight=1, minsize=50)
             for col in range(9):            
                 button = tk.Button(
                     master=grid_frame,
                     text='',
-                    font=font.Font(size=36, weight='bold'),
+                    font=font.Font(size=18, weight='bold'),
                     fg='black',
-                    width=3,
-                    height=2,
-                    highlightbackground='lightblue',
+                    bg='white',
+                    relief='solid',
+                    borderwidth=1,
                 )
                 self._cells[button] = (row, col)
                 button.grid(
                     row=row,
                     column=col,
-                    padx=5,
-                    pady=5,
+                    padx=0,
+                    pady=0,
                     sticky="nsew"
                 )
 
 def main():
     board = SudokuBoard()
+    board.geometry('400x500')
     board.mainloop()
 
 if __name__ == '__main__':
